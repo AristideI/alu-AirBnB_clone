90967414b97d418354dd5d567949f8f901b37b01
AristideI <a.isingizwe@alustudent.com>
2023-09-06 21:49:44 +0200

    updates

--- models/state.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" Class state that inherits from base model"""
-
-from models.base_model import BaseModel
-
-
-class State(BaseModel):
-    """ Class State  that inherits from base model """
-    name = ""
